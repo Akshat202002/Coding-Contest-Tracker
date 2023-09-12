943b0a11d21d2758c4b5990b6d1cbaa99772b539
Akshat202002 <akshatsamdani23@gmail.com>
2023-09-13 01:51:02 +0530

    Updated time format

--- coding-contest-tracker-app/src/components/ContestsCoulmns.js
+++ coding-contest-tracker-app/src/components/ContestsCoulmns.js
@@ -10,32 +10,43 @@ function ContestColumns({ liveContests, todayContests, upcomingContests }) {
         return startTime <= currentTime && currentTime <= endTime;
     };
 
+    const formatDate = (time) => {
+        const options = { weekday: 'short', year: '2-digit', month: '2-digit', day: '2-digit' };
+        return new Date(time).toLocaleDateString('en-US', options);
+    };
+
     const renderTimeBox = (time, isStart) => (
         <div className={`rounded-md p-1 text-white ${isStart ? 'bg-green-500' : 'bg-blue-500'}`}>
-            {new Date(time).toLocaleTimeString()}
+            {new Date(time).toLocaleTimeString('en-US', { hour12: false })}
         </div>
     );
 
     const renderContestCard = (contest) => (
         <div
             key={contest.name}
-            className="bg-white rounded-lg shadow-md p-4 mb-4 transition duration-300 hover:shadow-lg w-full text-center"
+            className="bg-white rounded-lg shadow-md p-4 mb-4 transition duration-300 hover:shadow-lg w-full"
         >
-            <a
-                href={contest.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline text-lg font-semibold"
-            >
-                {contest.name}
-            </a>
-            <div className="flex items-center justify-end mt-2">
-                <span className="text-gray-600 font-semibold">Start Time:</span>
-                {renderTimeBox(contest.start_time, true)}
+            <div className="flex flex-col md:flex-row items-center justify-between">
+                <a
+                    href={contest.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline text-lg font-semibold md:text-xl md:mr-4 text-center md:text-left"
+                >
+                    {contest.name}
+                </a>
+                <div className="flex flex-col items-center md:items-end mt-2 md:mt-0">
+                    <span>{formatDate(contest.start_time)}</span>
+                    <span>{formatDate(contest.end_time)}</span>
+                </div>
             </div>
-            <div className="flex items-center justify-end mt-2">
-                <span className="text-gray-600 font-semibold">End Time:</span>
-                {renderTimeBox(contest.end_time, false)}
+            <div className="flex items-center mt-2">
+                <div className="flex flex-col items-center md:items-end">
+                    {renderTimeBox(contest.start_time, true)}
+                </div>
+                <div className="flex flex-col items-center md:items-end ml-4">
+                    {renderTimeBox(contest.end_time, false)}
+                </div>
             </div>
         </div>
     );
@@ -67,28 +78,10 @@ function ContestColumns({ liveContests, todayContests, upcomingContests }) {
                     Upcoming Contests
                 </button>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 p-2"> {/* One contest per row */}
-                {activeView === 'today' && (
-                    todayContests.map((contest) => (
-                        <div key={contest.name} className="md:w-full"> {/* Set width for one contest */}
-                            {renderContestCard(contest)}
-                        </div>
-                    ))
-                )}
-                {activeView === 'live' && (
-                    liveContestsList.map((contest) => (
-                        <div key={contest.name} className="md:w-full"> {/* Set width for one contest */}
-                            {renderContestCard(contest)}
-                        </div>
-                    ))
-                )}
-                {activeView === 'upcoming' && (
-                    upcomingContests.map((contest) => (
-                        <div key={contest.name} className="md:w-full"> {/* Set width for one contest */}
-                            {renderContestCard(contest)}
-                        </div>
-                    ))
-                )}
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 p-2">
+                {activeView === 'today' && todayContests.map((contest) => renderContestCard(contest))}
+                {activeView === 'live' && liveContestsList.map((contest) => renderContestCard(contest))}
+                {activeView === 'upcoming' && upcomingContests.map((contest) => renderContestCard(contest))}
             </div>
         </div>
     );
